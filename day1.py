ee7e6bf83f915920441a4f423900e8b8fabbda51
ryanlatch <ryanlatch@hotmail.co.uk>
2024-12-02 16:31:12 +0000

    day 1 part 2

--- day1.py
+++ day1.py
@@ -12,8 +12,8 @@ with open("numbers.txt", "r") as file:
     for line in lines:
         # split the lines by the whitespace and add the list to the list
         num = line.split()
-        list1.append(num[0])
-        list2.append(num[1])
+        list1.append(int(num[0]))
+        list2.append(int(num[1]))
 
 # sort the lists
 list1 = sorted(list1)
@@ -24,7 +24,17 @@ totalDistance = 0
 
 for i in range(len(list1)):
     # abs gets the number rather than + or -. Also cast to int as theyre strs
-    diff = abs(int(list1[i]) - int(list2[i]))
+    diff = abs(list1[i] - list2[i])
     totalDistance += diff
 
-print(totalDistance)
+print("total distance: ", totalDistance)
+
+# part 2
+
+final_similarity_score = 0
+
+# check every number in the list and * by the amount of times it occurs in the 2nd list
+for num in list1:
+    final_similarity_score += num * list2.count(num)
+
+print("similarity score: ", final_similarity_score)
